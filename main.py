02608de8dee407dbbee75ab67e653e038ed1508b
Arpit Kadam <arpitkadam922@gmail.com>
2024-12-29 00:03:06 +0530

    Data Transformation Stage Commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataValidationConfig, DataIngestionConfig
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import DataValidationConfig, DataIngestionConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 import sys 
@@ -26,7 +27,13 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info(">>>>>>>>>>>>>Data Validation Completed<<<<<<<<<<<<<<")
         print(data_validation_artifact)
-        
+
+        logging.info(">>>>>>>>>>>>>Data Transformation Started<<<<<<<<<<<<<<")
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info(">>>>>>>>>>>>>Data Transformation Completed<<<<<<<<<<<<<<")
+        print(data_transformation_artifact)
     except NetworkSecurityException as e:
         raise NetworkSecurityException(e,sys)
     
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -23,3 +23,25 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path: str, array: np.array):
+    try:
+        logging.info(f"Saving numpy array to file: {file_path}")
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+        logging.info(f"Complete saving numpy array to file: {file_path}")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info(f"Saving object to file: {file_path}")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info(f"Complete saving object to file: {file_path}")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
